0d5eeb59d58cc6b245f59e9e20709fe69b1635e0
pbhootra-sf <47865825+pbhootra-sf@users.noreply.github.com>
2023-11-08 02:11:56 +0530

    Update scan/integrationTest.java
    Co-authored-by: ast-source-code-scanning[bot] <149949854+ast-source-code-scanning[bot]@users.noreply.github.com>

--- scan/integrationTest.java
+++ scan/integrationTest.java
@@ -54,7 +54,7 @@ public class SnykIntegrationManagerTest {
 				"}]";
 		ObjectMapper mapper = new ObjectMapper();
 		mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
 		JsonNode node1 = mapper.readTree(s);
 		ArrayNode a =  (ArrayNode) node1;
 		when(astSnyk.getOrgs(anyString())).thenReturn(a);
